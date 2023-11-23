9df306036c575b0c86552db3c5c6a484490f14fc
Ahmed Saber Alsayed <aahmedo284@gmail.com>
2023-11-23 21:42:09 +0200

    secondcommit

---
+++ database/migrations/2023_11_23_185306_create_rooms_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->integer('room_cost');
+            $table->integer('room_number');
+            $table->integer('room_availability');
+            $table->enum('room_type',['single','Double','King','Twin']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
---
+++ database/migrations/2023_11_23_185355_create_bills_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->id();
+            $table->double('total_amount');
+            $table->dateTime('date_from');
+            $table->dateTime('date_to');
+            $table->string('payment_method');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bills');
+    }
+};
